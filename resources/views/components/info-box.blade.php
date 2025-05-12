e2c1c9dd70b4aa340c054366dd20ec1e5d572e2d
Narek <narek.dev13@gmail.com>
2025-05-12 10:36:37 +0400

    changes

--- resources/views/components/info-box.blade.php
+++ resources/views/components/info-box.blade.php
@@ -3,14 +3,6 @@
         <p class="mb-4">
             1. <a style="color: blue !important; text-decoration: underline;" href="{{ route('download.code') }}" download="code.php" class="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600">Загрузите этот файл</a>
         </p>
-        <p class="mb-4">2. Скопируйте этот токен</p>
-        <p class="font-mono text-lg bg-gray-100 dark:bg-gray-700 p-2 rounded">{{
-            $this->data['token'] }}</p>
-
-        <p class="mb-4">3. Откройте загруженный файл code.php</p>
-        <p class="mb-4">4. Вставьте скопированный токен в переменную TOKEN</p>
-        <p class="mb-4">5. Добавьте файл code.php в основной каталог вашего проекта (например /public_html/code.php)</p>
-        <p class="mb-4">6. Запустите файл code.php с помощью команды php code.php (при необходимости можно использовать Supervisor для запуска в фоновом режиме)</p>
-
+        <p class="mb-4">2.Разместите его в корневой директории вашего веб-сайта. Убедитесь, что он доступен по следующему адресу: https://ваш-домен.ru/check.php</p>
     </div>
 </div>
